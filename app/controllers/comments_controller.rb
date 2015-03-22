7f954816a1c5b7e37dc1eda63872e4076f5fb820
jasandine <jasandine@gmail.com>
2015-03-22 14:12:27 -0700

    Create add comments to a post functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment Added"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+
+
+
+private
+  def comment_params
+    params.require(:comment).permit(:text, :post_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  root :to => 'posts#index'
+  root :to => "lists#index"
   get '/new_post' => 'posts#new'
   post '/new_post' => 'posts#create'
+  resources :posts do 
+    resources :comments, :except => [:show, :index]
+end
 end
