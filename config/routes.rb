06d573ab1ee33d6d477cae15598804006ee79824
jasandine <jasandine@gmail.com>
2015-03-27 12:25:19 -0700

    Set up login logout functionality/views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root :to => "posts#index"
   get '/new_post' => 'posts#new'
   post '/new_post' => 'posts#create'
+  resources :users 
   resources :posts do 
     resources :comments, :except => [:show, :index]
 end
