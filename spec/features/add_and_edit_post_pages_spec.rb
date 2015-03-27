974d2469a7e2b955900fcc1ff604d6003be29ca5
jasandine <jasandine@gmail.com>
2015-03-27 11:07:21 -0700

    Add delete post spec

--- spec/features/add_and_edit_post_pages_spec.rb
+++ spec/features/add_and_edit_post_pages_spec.rb
@@ -23,4 +23,15 @@ describe "the edit post process" do
   end
 end
 
+describe "the delete post process" do
+  it 'deletes an existing post' do
+    post = Post.create(:title => "MY OPIONIONS", :text => "SO MANY!")
+    visit root_path
+    click_on 'MY OPIONIONS'
+    click_on 'Delete Post'
+    expect(page).to have_content "Post deleted"
+  end
+end
+
+
 
