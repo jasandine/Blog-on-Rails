0acf1b5a42acdd4a55f0c155549422b661e9b871
jasandine <jasandine@gmail.com>
2015-03-27 15:55:32 -0700

    Push to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,17 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+end
 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 def sign_in(user)
-  visit new_user_session_path
-  fill_in "Email", with: user.email
-  fill_in "Password", with: user.password
-  click_on "Log in"
+   user.confirm!
+   visit new_user_session_path
+   fill_in "Email",    with: user.email
+   fill_in "Password", with: user.password
+   click_button "Sign in"
+end
+
+def login_user
+  Warden.test_mode!
+  user = create(:user)
+  login_as user, :scope => :user
+  user.confirmed_at = Time.now
+  user.confirm!
+  user.save
+  user
 end
